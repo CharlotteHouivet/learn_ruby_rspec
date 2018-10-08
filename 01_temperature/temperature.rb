2bfde08650b95743e6376446ba50339881082ee3
CharlotteHouivet <43582375+CharlotteHouivet@users.noreply.github.com>
2018-10-08 21:54:23 +0200

    upload code

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,13 @@
-#write your code here
+def ftoc(fahrenheit)
+  celsius = (fahrenheit.to_f - 32) * 5.0 / 9.0
+  celsius.round (1)
+end
+
+def ctof(celsius)
+  fahrenheit = (celsius.to_f * 9.0 / 5) + 32
+  fahrenheit.round (1)
+end
+
+
+
+#.round(x) Pour arrondir à x décimales
