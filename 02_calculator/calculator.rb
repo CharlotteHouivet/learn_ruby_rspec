41afe9a9f1bc3644953c896c50ae419e648b7df8
CharlotteHouivet <43582375+CharlotteHouivet@users.noreply.github.com>
2018-10-08 21:55:53 +0200

    upload code

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,31 @@
-#write your code here
+def add(x, y)
+	p x + y
+end
+
+def subtract(x, y)
+	p x - y
+end
+
+def sum(array)
+	sum = 0				#définit la variable sum = 0 (si rien, "nil")		
+	array.each do |n|	#pour chaque élément de mon array
+		sum = sum + n 	#le résultat est une addition
+	end
+	p sum
+end
+
+def multiply(x, y)
+	p x * y
+end
+
+def power(x, y)
+	p x ** y
+end
+
+def factorial(n)
+	if 	n == 0 || n == 1
+	p 1
+	else (2..n).inject(:*) 
+end
+end
+
