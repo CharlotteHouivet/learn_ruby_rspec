4d781f65bdfb7a7857aafd267b244a9b16f44483
CharlotteHouivet <43582375+CharlotteHouivet@users.noreply.github.com>
2018-10-08 22:10:32 +0200

    added comments

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -8,8 +8,8 @@ end
 
 def sum(array)
 	sum = 0				#définit la variable sum = 0 (si rien, "nil")		
-	array.each do |n|	#pour chaque élément de mon array
-		sum = sum + n 	#le résultat est une addition
+	array.each do |n|		#pour chaque élément de mon array
+		sum = sum + n 		#le résultat est une addition
 	end
 	p sum
 end
@@ -23,9 +23,9 @@ def power(x, y)
 end
 
 def factorial(n)
-	if 	n == 0 || n == 1
+	if 	n == 0 || n == 1	#exceptions sur 0! et 1!
 	p 1
-	else (2..n).inject(:*) 
+	else (2..n).inject(:*) 		#on commence donc à 2
 end
 end
 
