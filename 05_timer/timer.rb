f4c8230206df6a85f60718fee55115c3476e72e8
CharlotteHouivet <43582375+CharlotteHouivet@users.noreply.github.com>
2018-10-08 22:04:52 +0200

    upload code

--- 05_timer/timer.rb
+++ 05_timer/timer.rb
@@ -1,1 +1,7 @@
-#write your code here
+def time_string(sec)
+	return Time.at(sec).utc.strftime("%H:%M:%S")
+end
+
+#Time = classe Ruby prédéfinie
+#utc = faire commencer les calculs à 0
+#%H:%M:%S = format HH:MM:SS
