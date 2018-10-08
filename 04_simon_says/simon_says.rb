3608538c62ca74690b3abf9f1491170b9a0a20a2
CharlotteHouivet <43582375+CharlotteHouivet@users.noreply.github.com>
2018-10-08 22:03:06 +0200

    upload code

--- 04_simon_says/simon_says.rb
+++ 04_simon_says/simon_says.rb
@@ -1,1 +1,25 @@
-#write your code here
+def echo (a)
+	return a
+end
+
+def shout (a)
+	return a.upcase
+end
+
+def repeat (a, b = 2) 				#où a est le mot et b le nombre de répétition
+	return ((a + " ") * b).rstrip
+end
+
+def start_of_word(word, x)
+	word[0...x]
+end
+
+def first_word(string)
+	string.split.first
+end
+
+def titleize(var)
+    lowercase_var = %w{and over the}
+    var.split.each_with_index.map{|s, index|
+    lowercase_var.include?(s) && index > 0 ? s : s.capitalize}.join(" ")
+end
